01d18e2ea56af7a03d16948d72636e1b58b34bea
Lukáš Úradník <82445388+Krypt0niT@users.noreply.github.com>
2025-03-21 16:30:38 +0100

    Math.Test: Operation tests separatesd.

--- src/CalculatorApp.Math.Tests/SimpleOperationTests/SumOperationTests.cs
+++ src/CalculatorApp.Math.Tests/SimpleOperationTests/SumOperationTests.cs
@@ -1,8 +1,9 @@
 using CalculatorApp.MathLibrary.Entities;
+using CalculatorApp.MathLibrary.Entities.AbsoluteMembers;
 using CalculatorApp.MathLibrary.Entities.Operands;
 using FluentAssertions;
 
-namespace CalculatorApp.Math.Tests;
+namespace CalculatorApp.Math.Tests.SimpleOperationTests;
 
 public class SumOperationTests
 {
@@ -20,7 +21,7 @@ public class SumOperationTests
 
         var result = input.GetResult();
 
-        result.Should().Be(3m);
+        result.Should().Be(3.0m);
     }
 
     [Fact]
@@ -41,12 +42,44 @@ public class SumOperationTests
                 ),
                 new AbsoluteMember(15),
                 new AbsoluteMember(1),
-
             }
         );
 
         var result = input.GetResult();
 
+        // 1 + 5 + 5 + 15 + 1
         result.Should().Be(27m);
+    }
+
+    [Fact]
+    public void EmptySumTest()
+    {
+        var input = new SumOperation
+        (
+            new List<Term>
+            {
+            }
+        );
+
+        var result = input.GetResult();
+
+        result.Should().Be(0);
+    }
+
+    [Fact]
+    public void FloatingErrorSumTest()
+    {
+        var input = new SumOperation
+        (
+            new List<Term>
+            {
+                new AbsoluteMember(0.1m),
+                new AbsoluteMember(0.2m),
+            }
+        );
+
+        var result = input.GetResult();
+
+        result.Should().Be(0.3m);
     }
 }
