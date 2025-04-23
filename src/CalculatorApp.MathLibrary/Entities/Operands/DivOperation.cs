a371f3a37c9d57a6a4c44276703d2c15846361d1
katewins420 <85704088+katewins420@users.noreply.github.com>
2025-04-24 01:53:14 +0200

    Math: DivOperation implemented.

--- src/CalculatorApp.MathLibrary/Entities/Operands/DivOperation.cs
+++ src/CalculatorApp.MathLibrary/Entities/Operands/DivOperation.cs
@@ -8,6 +8,22 @@ public class DivOperation : Term
 
     public override decimal GetResult()
     {
-        throw new NotImplementedException();
+        decimal result = 0;
+        if (InnerTerms.Count == 0)
+            return 0;
+
+        if (InnerTerms.Count == 1)
+        {
+            result = InnerTerms.First().GetResult();
+            return result;
+        }
+
+        result = InnerTerms.First().GetResult();
+        for (int i = 1; i < InnerTerms.Count; i++)
+        {
+            result /= InnerTerms[i].GetResult();
+        }
+
+        return result;
     }
 }
