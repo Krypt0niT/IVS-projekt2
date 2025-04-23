36a272701fa4793a4bcc60832a4047e13e1e35f1
katewins420 <85704088+katewins420@users.noreply.github.com>
2025-04-24 01:09:49 +0200

    Math.Tests: Simple MulOperation tests added.

---
+++ src/CalculatorApp.Math.Tests/SimpleOperationTests/MulOperationTests.cs
@@ -0,0 +1,102 @@
+﻿using CalculatorApp.MathLibrary.Entities.AbsoluteMembers;
+using CalculatorApp.MathLibrary.Entities.Operands;
+using CalculatorApp.MathLibrary.Entities;
+using FluentAssertions;
+
+namespace CalculatorApp.Math.Tests.SimpleOperationTests;
+    public class MulOperationTests
+    {
+        [Fact]
+        public void SingleMulTest()
+        {
+            var input = new MulOperation
+            (
+                new List<Term>
+                {
+                    new AbsoluteMember(1),
+                }
+            );
+
+            var result = input.GetResult();
+
+            result.Should().Be(1);
+        }
+
+        [Fact]
+        public void MultipleMulTest()
+        {
+            var input = new MulOperation
+            (
+                new List<Term>
+                {
+                        new AbsoluteMember(1),
+                        new AbsoluteMember(2),
+                        new AbsoluteMember(-2),
+                }
+            );
+
+            var result = input.GetResult();
+
+            result.Should().Be(-4m);
+        }
+
+        [Fact]
+        public void DepthMulTest()
+        {
+            var input = new MulOperation
+            (
+                new List<Term>
+                {
+                    new AbsoluteMember(1),
+                    new MulOperation
+                    (
+                        new List<Term>
+                        {
+                            new AbsoluteMember(5),
+                            new AbsoluteMember(5),
+                        }
+                    ),
+                    new AbsoluteMember(-5),
+                    new AbsoluteMember(-1),
+                }
+            );
+
+            var result = input.GetResult();
+
+            // 1 - 5 - 5 + 15 - 1
+            result.Should().Be(125m);
+        }
+
+        [Fact]
+        public void EmptyMulTest()
+        {
+            var input = new MulOperation
+            (
+                new List<Term>
+                {
+                }
+            );
+
+            var result = input.GetResult();
+
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void FloatingMulTest()
+        {
+            var input = new MulOperation
+            (
+                new List<Term>
+                {
+                    new AbsoluteMember(0.1m),
+                    new AbsoluteMember(0.2m),
+                }
+            );
+
+            var result = input.GetResult();
+
+            result.Should().Be(0.02m);
+        }
+    }
+
