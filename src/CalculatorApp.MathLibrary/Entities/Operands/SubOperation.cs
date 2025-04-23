73da63c0aa62df95ac317eff8babb1861b0fa58a
katewins420 <85704088+katewins420@users.noreply.github.com>
2025-04-24 00:55:24 +0200

    Math: Sub operation fixed for one AbsoluteMember.

--- src/CalculatorApp.MathLibrary/Entities/Operands/SubOperation.cs
+++ src/CalculatorApp.MathLibrary/Entities/Operands/SubOperation.cs
@@ -8,10 +8,17 @@ public class SubOperation : Term
 
     public override decimal GetResult()
     {
+        decimal result = 0;
         if (InnerTerms.Count == 0)
             return 0;
 
-        decimal result = InnerTerms[0].GetResult();
+        if (InnerTerms.Count == 1)
+        {
+            result -= InnerTerms.First().GetResult();
+            return result;
+        }
+
+        result = InnerTerms.First().GetResult();
         for (int i = 1; i < InnerTerms.Count; i++)
         {
             result -= InnerTerms[i].GetResult();
