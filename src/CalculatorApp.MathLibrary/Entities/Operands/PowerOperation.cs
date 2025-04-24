495363d315417abdb076decd3a10e56b21bfa1b9
katewins420 <85704088+katewins420@users.noreply.github.com>
2025-04-24 13:20:23 +0200

    Math: PowerOperation implemented.

--- src/CalculatorApp.MathLibrary/Entities/Operands/PowerOperation.cs
+++ src/CalculatorApp.MathLibrary/Entities/Operands/PowerOperation.cs
@@ -11,6 +11,26 @@ public class PowerOperation : Term
 
     public override decimal GetResult()
     {
-        throw new NotImplementedException();
+        if (InnerTerms!.Count == 0)
+            return 0;
+
+        if (InnerTerms.Count != 1)
+            throw new NotSupportedException();
+
+        decimal baseValue = InnerTerms.First().GetResult();
+
+        return DecimalPow(baseValue, Exponent);
+    }
+
+    private decimal DecimalPow(decimal baseValue, uint exponent)
+    {
+        decimal result = 1;
+
+        for (uint i = 0; i < exponent; i++)
+        {
+            result *= baseValue;
+        }
+
+        return result;
     }
 }
