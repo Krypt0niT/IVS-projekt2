e013b0542af543bf605190dc22ff799200fac9f1
katewins420 <85704088+katewins420@users.noreply.github.com>
2025-04-24 10:29:31 +0200

    Math: FactorialOperation implemented.

--- src/CalculatorApp.MathLibrary/Entities/Operands/FactorialOperation.cs
+++ src/CalculatorApp.MathLibrary/Entities/Operands/FactorialOperation.cs
@@ -8,6 +8,25 @@ public class FactorialOperation : Term
 
     public override decimal GetResult()
     {
-        throw new NotImplementedException();
+        if (InnerTerms!.Count == 0)
+            return 0;
+
+        if (InnerTerms.Count != 1)
+            throw new NotSupportedException();
+
+        decimal input = InnerTerms.First().GetResult();
+
+        if (input < 0 || input % 1 != 0)
+            throw new NotSupportedException();
+
+        int n = (int)input;
+
+        decimal result = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            result *= i;
+        }
+
+        return result;
     }
 }
