85de496f5a742ac54b9aadbb721c2b900dd0d45d
katewins420 <85704088+katewins420@users.noreply.github.com>
2025-04-24 02:23:00 +0200

    Math.Tests: Simple AbsOperation tests added.

---
+++ src/CalculatorApp.Math.Tests/SimpleOperationTests/AbsOperationTests.cs
@@ -0,0 +1,100 @@
+﻿using CalculatorApp.MathLibrary.Entities.AbsoluteMembers;
+using CalculatorApp.MathLibrary.Entities.Operands;
+using CalculatorApp.MathLibrary.Entities;
+using FluentAssertions;
+
+namespace CalculatorApp.Math.Tests.SimpleOperationTests;
+    public class AbsOperationTests
+    {
+        [Fact]
+        public void SingleAbsTest()
+        {
+            var input = new AbsOperation
+            (
+                new List<Term>
+                {
+                    new AbsoluteMember(10),
+                }
+            );
+
+            var result = input.GetResult();
+
+            result.Should().Be(10);
+        }
+
+        [Fact]
+        public void MultipleAbsErrorTest()
+        {
+            var input = new AbsOperation
+            (
+                new List<Term>
+                {
+                    new AbsoluteMember(10),
+                    new AbsoluteMember(15),
+                    new AbsoluteMember(1),
+                }
+            );
+
+        Assert.Throws<NotSupportedException>(() =>
+        {
+            var result = input.GetResult();
+        });
+
+    }
+
+
+        [Fact]
+        public void DepthAbsTest()
+        {
+            var input = new AbsOperation
+            (
+                new List<Term>
+                {
+                    new AbsOperation
+                    (
+                        new List<Term>
+                        {
+                            new AbsoluteMember(-5),
+                        }
+                    ),
+                }
+            );
+
+            var result = input.GetResult();
+
+            result.Should().Be(5m);
+        }
+
+        [Fact]
+        public void EmptyAbsTest()
+        {
+            var input = new AbsOperation
+            (
+                new List<Term>
+                {
+                }
+            );
+
+            var result = input.GetResult();
+
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void FloatingAbsTest()
+        {
+            var input = new AbsOperation
+            (
+                new List<Term>
+                {
+                    new AbsoluteMember(-0.2m)
+                }
+            );
+
+            var result = input.GetResult();
+
+            result.Should().Be(0.2m);
+        }
+    }
+
+
