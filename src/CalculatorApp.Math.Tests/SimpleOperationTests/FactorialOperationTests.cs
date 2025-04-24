d9c68ab267a5040c419491880c47486146c1f340
katewins420 <85704088+katewins420@users.noreply.github.com>
2025-04-24 10:30:56 +0200

    Math.Tests: Simple FactorialOperation tests added.

---
+++ src/CalculatorApp.Math.Tests/SimpleOperationTests/FactorialOperationTests.cs
@@ -0,0 +1,100 @@
+﻿using CalculatorApp.MathLibrary.Entities.AbsoluteMembers;
+using CalculatorApp.MathLibrary.Entities.Operands;
+using CalculatorApp.MathLibrary.Entities;
+using FluentAssertions;
+
+namespace CalculatorApp.Math.Tests.SimpleOperationTests;
+    public class FactorialOperationTests
+    {
+    [Fact]
+    public void SingleFactorialTest()
+    {
+        var input = new FactorialOperation
+        (
+            new List<Term>
+            {
+                    new AbsoluteMember(5),
+            }
+        );
+
+        var result = input.GetResult();
+
+        result.Should().Be(120);
+    }
+
+    [Fact]
+    public void MultipleFactorialErrorTest()
+    {
+        var input = new FactorialOperation
+        (
+            new List<Term>
+            {
+                    new AbsoluteMember(10),
+                    new AbsoluteMember(15),
+                    new AbsoluteMember(1),
+            }
+        );
+
+        Assert.Throws<NotSupportedException>(() =>
+        {
+            var result = input.GetResult();
+        });
+
+    }
+
+
+    [Fact]
+    public void DepthFactorialTest()
+    {
+        var input = new FactorialOperation
+        (
+            new List<Term>
+            {
+                    new FactorialOperation
+                    (
+                        new List<Term>
+                        {
+                            new AbsoluteMember(3),
+                        }
+                    ),
+            }
+        );
+
+        var result = input.GetResult();
+
+        result.Should().Be(720m);
+    }
+
+    [Fact]
+    public void EmptyFactorialTest()
+    {
+        var input = new FactorialOperation
+        (
+            new List<Term>
+            {
+            }
+        );
+
+        var result = input.GetResult();
+
+        result.Should().Be(0);
+    }
+
+    [Fact]
+    public void FloatingFactorialErrorTest()
+    {
+        var input = new FactorialOperation
+        (
+            new List<Term>
+            {
+                    new AbsoluteMember(0.5m),
+            }
+        );
+
+        Assert.Throws<NotSupportedException>(() =>
+        {
+            var result = input.GetResult();
+        });
+    }
+}
+
