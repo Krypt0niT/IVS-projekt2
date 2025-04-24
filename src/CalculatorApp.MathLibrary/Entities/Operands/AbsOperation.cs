a77d01efdb0355c005b1b59fa19e9d164b5642ff
katewins420 <85704088+katewins420@users.noreply.github.com>
2025-04-24 02:22:02 +0200

    Math: AbsOperation implemented.

--- src/CalculatorApp.MathLibrary/Entities/Operands/AbsOperation.cs
+++ src/CalculatorApp.MathLibrary/Entities/Operands/AbsOperation.cs
@@ -4,11 +4,20 @@ public class AbsOperation : Term
 {
     public AbsOperation(IList<Term> terms) : base(terms)
     {
-        if (terms.Count > 1) throw new NotSupportedException();
+        //if (terms.Count > 1) throw new NotSupportedException();
     }
 
     public override decimal GetResult()
     {
-        throw new NotImplementedException();
+        if (InnerTerms.Count == 0)
+            return 0;
+
+        if (InnerTerms.Count > 1)
+            throw new NotSupportedException();
+
+        decimal result = InnerTerms.First().GetResult();
+        if (result < 0)
+            result *= -1;
+        return result;
     }
 }
