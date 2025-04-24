681f3b628fddf4d529966713ef7ffe44535368c8
katewins420 <85704088+katewins420@users.noreply.github.com>
2025-04-24 13:54:32 +0200

    Math: SquareRootOperation implemented.

--- src/CalculatorApp.MathLibrary/Entities/Operands/SquareRootOperation.cs
+++ src/CalculatorApp.MathLibrary/Entities/Operands/SquareRootOperation.cs
@@ -1,4 +1,6 @@
-﻿namespace CalculatorApp.MathLibrary.Entities.Operands;
+﻿using CalculatorApp.MathLibrary.Entities.AbsoluteMembers;
+
+namespace CalculatorApp.MathLibrary.Entities.Operands;
 
 public class SquareRootOperation : Term
 {
@@ -11,6 +13,50 @@ public class SquareRootOperation : Term
 
     public override decimal GetResult()
     {
-        throw new NotImplementedException();
+        if(InnerTerms!.Count == 0)
+            return 0;
+        
+        if (InnerTerms!.Count != 1)
+            throw new NotSupportedException();
+
+        decimal input = InnerTerms.First().GetResult();
+
+        if (input < 0 && Degree % 2 == 0)
+            throw new NotSupportedException();
+
+        if (input == 0)
+            return 0;
+
+        return NthRoot(input, Degree, 0.000001m); // precision = 1e-6
     }
+
+    private decimal NthRoot(decimal value, uint n, decimal precision)
+    {
+        if (n == 0)
+            throw new NotSupportedException();
+
+        decimal x = value / n;
+        decimal prev = 0;
+
+        while (new AbsOperation(
+            new List<Term> {
+                new SubOperation(new List<Term> {
+                    new AbsoluteMember(x),
+                    new AbsoluteMember(prev)
+                })
+            }
+        ).GetResult() > precision)
+        {
+            prev = x;
+
+            var pow = new PowerOperation(n - 1, new List<Term> {
+            new AbsoluteMember(x)
+        }).GetResult();
+
+            x = ((n - 1) * x + value / pow) / n;
+        }
+
+        return x;
+    }
+
 }
