fba9a2bc95beecc23af722c9079bf22fe3de96a9
katewins420 <85704088+katewins420@users.noreply.github.com>
2025-04-24 21:33:14 +0200

    Math.Constants: PiConst implemented.

--- src/CalculatorApp.MathLibrary/Entities/AbsoluteMembers/Constants/PiConst.cs
+++ src/CalculatorApp.MathLibrary/Entities/AbsoluteMembers/Constants/PiConst.cs
@@ -1,13 +1,24 @@
 ﻿namespace CalculatorApp.MathLibrary.Entities.AbsoluteMembers;
 
+/// <summary>
+/// Represents the mathematical constant π (pi).
+/// </summary>
 public class PiConst : Term
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PiConst"/> class.
+    /// </summary>
+    /// <param name="terms">Optional list of inner terms (not used).</param>
     public PiConst(IList<Term>? terms = null) : base(terms)
     {
     }
 
+    // <summary>
+    /// Returns the decimal approximation of the constant π (pi).
+    /// </summary>
+    /// <returns>Decimal value of π (pi), approximately 3.14159265358979.</returns>
     public override decimal GetResult()
     {
-        throw new NotImplementedException();
+        return 3.14159265358979m;
     }
 }
