37bb278640fa3b0aaa76ffc63ead2581615fb3ef
katewins420 <85704088+katewins420@users.noreply.github.com>
2025-04-24 02:41:35 +0200

    Math: ModuloOperation implemented.

--- src/CalculatorApp.MathLibrary/Entities/Operands/ModuloOperation.cs
+++ src/CalculatorApp.MathLibrary/Entities/Operands/ModuloOperation.cs
@@ -8,6 +8,21 @@ public class ModuloOperation : Term
 
     public override decimal GetResult()
     {
-        throw new NotImplementedException();
+        if (InnerTerms!.Count == 0)
+            return 0;
+
+        if (InnerTerms.Count == 1)
+            return InnerTerms.First().GetResult();
+
+        if (InnerTerms.Count != 2)
+            throw new NotSupportedException();
+
+        if (InnerTerms.Last().GetResult() == 0)
+            throw new DivideByZeroException();
+
+        decimal result = 0;
+        result = InnerTerms.First().GetResult() % InnerTerms.Last().GetResult();
+        return result;
+
     }
 }
