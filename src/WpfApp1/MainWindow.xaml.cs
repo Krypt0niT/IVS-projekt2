e25a548346d38b1d24e17c0327bfc6023f94a3d0
Lukáš Úradník <82445388+Krypt0niT@users.noreply.github.com>
2025-04-28 15:43:40 +0200

    Wpf: Test UI changes.

--- src/WpfApp1/MainWindow.xaml.cs
+++ src/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using CalculatorApp.MathLibrary.Entities;
+using CalculatorApp.MathLibrary.Entities.AbsoluteMembers;
+using CalculatorApp.MathLibrary.Entities.Operands;
+using System.Windows;
 using System.Windows.Media;
+using XamlMath;
 
 namespace WpfApp1
 {
@@ -8,14 +12,42 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        SumOperation equation;
+
         public MainWindow()
         {
             InitializeComponent();
-            formulaWraper.Formula = @"\frac{a}{b} + \sqrt{c^2 + d^2} = e";
+            formulaWraper.Formula = @"\color{red}{|} -10  \color{green}{|}"
+;
             formulaWraper.FontSize = 30;  // Font
             formulaWraper.FontFamily = new System.Windows.Media.FontFamily("Arial");  // Font
-            formulaWraper.Foreground = new SolidColorBrush(Colors.Blue);  // Farba textu
-            formulaWraper.Background = new SolidColorBrush(Colors.Yellow); // Farba pozadia
+            formulaWraper.Foreground = new SolidColorBrush(Colors.Black);  // Farba textu
+
+
+            equation = new SumOperation
+            (
+                new List<Term>() { 
+                    new SubOperation(new List<Term>() 
+                    {
+                        new AbsoluteMember(1) { IsSelected = true},
+                        new AbsoluteMember(5)
+                    }),
+                    new NthRootOperation
+                    (
+                        5,
+                        new List<Term>()
+                        {
+                            new AbsoluteMember(5646)
+                        }
+                    ),
+                    new AbsoluteMember(2)
+                }
+            );
+            var sub = equation.InnerTerms.ElementAt(0);
+            var firts = sub.InnerTerms.ElementAt(0);
+
+            var result = equation.GetLateX();
+            formulaWraper.Formula = result;
         }
 
         private void WindowsFormsHost_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
@@ -28,9 +60,9 @@ namespace WpfApp1
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e) // +
         {
-
+            
         }
     }
 }
