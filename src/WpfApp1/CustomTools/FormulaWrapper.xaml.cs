49ed6ffacd70f9fd97c54692113ce02ec281b7d3
Lukáš Úradník <82445388+Krypt0niT@users.noreply.github.com>
2025-04-28 03:41:33 +0200

    Wpf: LateX functionality implemented in Wpf.

---
+++ src/WpfApp1/CustomTools/FormulaWrapper.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace WpfApp1
+{
+    public partial class FormulaWrapper : System.Windows.Controls.UserControl
+    {
+        public FormulaWrapper()
+        {
+            InitializeComponent();
+        }
+
+        // Môžeš pridať aj vlastnosti na dynamickú zmenu formulí
+        public string Formula
+        {
+            get { return formulaControl.Formula; }
+            set { formulaControl.Formula = value; }
+        }
+    }
+}
