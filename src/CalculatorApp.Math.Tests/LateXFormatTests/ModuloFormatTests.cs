a0cc6886fbaea6c0c139a937ec1f16b318b5ee23
katewins420 <85704088+katewins420@users.noreply.github.com>
2025-04-28 17:34:00 +0200

    Math.Tests.Format: ModuloFormatTests added.

---
+++ src/CalculatorApp.Math.Tests/LateXFormatTests/ModuloFormatTests.cs
@@ -0,0 +1,94 @@
+﻿using CalculatorApp.MathLibrary.Entities;
+using CalculatorApp.MathLibrary.Entities.AbsoluteMembers;
+using CalculatorApp.MathLibrary.Entities.Operands;
+using CalculatorApp.MathLibrary.LateXStyles;
+using FluentAssertions;
+
+
+namespace CalculatorApp.Math.Tests.LateXFormatTests;
+    public class ModuloFormatTests
+    {
+    [Fact]
+    public void SimpleModuloTest()
+    {
+        var input = new ModuloOperation
+        (
+            new List<Term>
+            {
+                    new AbsoluteMember(12),
+                    new AbsoluteMember(10)
+            }
+        );
+
+        var result = input.GetLateX();
+
+        var expectedLateX = @"12 \% 10";
+
+        result.Should().Be(expectedLateX);  // 12 % 10 Should be all black
+    }
+
+    [Fact]
+    public void SelectedFullModuloTest()
+    {
+        var input = new ModuloOperation
+        (
+            new List<Term>
+            {
+                    new AbsoluteMember(12),
+                    new AbsoluteMember(10)
+            }
+        );
+        input.IsSelected = true;
+
+
+        var result = input.GetLateX();
+
+        var expectedLateX = @"12 \colorbox{red}{\%} 10";
+
+        result.Should().Be(expectedLateX); //  Only % should be red
+    }
+
+    [Fact]
+    public void SelectedLeftMemberTest()
+    {
+        var input = new ModuloOperation
+        (
+            new List<Term>
+            {
+                    new AbsoluteMember(12)
+                    {
+                        IsSelected = true
+                    },
+                    new AbsoluteMember(10)
+            }
+        );
+
+        var result = input.GetLateX();
+
+        var expectedLateX = @"\colorbox{red}{12} \% 10";
+
+        result.Should().Be(expectedLateX); // Only left member should be red
+    }
+
+    public void SelectedRightMemberTest()
+    {
+        var input = new ModuloOperation
+        (
+            new List<Term>
+            {
+                    new AbsoluteMember(12),
+                    
+                    new AbsoluteMember(10){
+                        IsSelected = true
+                    }
+            }
+        );
+
+        var result = input.GetLateX();
+
+        var expectedLateX = @"12 \% \colorbox{red}{10}";
+
+        result.Should().Be(expectedLateX); // Only right member should be red
+    }
+}
+
