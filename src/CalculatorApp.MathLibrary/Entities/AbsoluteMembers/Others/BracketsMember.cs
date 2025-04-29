bebd9c7769260bd279900f53ebef4d5f74c946c7
Lukáš Úradník <82445388+Krypt0niT@users.noreply.github.com>
2025-04-29 22:31:50 +0200

    Math: BracketsMember added.

---
+++ src/CalculatorApp.MathLibrary/Entities/AbsoluteMembers/Others/BracketsMember.cs
@@ -0,0 +1,27 @@
+﻿using CalculatorApp.MathLibrary.LateXStyles;
+
+namespace CalculatorApp.MathLibrary.Entities.AbsoluteMembers;
+
+public class BracketsMember : Term
+{
+    public BracketsMember(IList<Term>? terms = null) : base(terms)
+    {
+    }
+
+    public override decimal GetResult()
+    {
+        if (InnerTerms == null || InnerTerms.Count != 1) throw new NotSupportedException();
+
+        var result = InnerTerms.First().GetResult();
+        return result;
+    }
+
+    public override string GetLateX()
+    {
+        if (InnerTerms == null || InnerTerms.Count != 1) throw new NotSupportedException();
+
+        var innerLateX = InnerTerms.First().GetLateX();
+        if (IsSelected) return $"\\colorbox{{{Colors.Highlight}}}{{(}}{innerLateX}\\colorbox{{{Colors.Highlight}}}{{)}}";
+        return $"({innerLateX})";
+    }
+}
