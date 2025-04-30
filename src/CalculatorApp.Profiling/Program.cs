43fa15177d925bd0b180794712f84e7875be45c1
Lukáš Úradník <82445388+Krypt0niT@users.noreply.github.com>
2025-04-30 15:24:22 +0200

    Profiling: Project init.

---
+++ src/CalculatorApp.Profiling/Program.cs
@@ -0,0 +1,21 @@
+﻿using CalculatorApp.MathLibrary.Entities;
+using CalculatorApp.MathLibrary.Entities.AbsoluteMembers;
+using CalculatorApp.MathLibrary.Entities.Operands;
+
+Console.WriteLine("Hello, World!");
+
+
+// najprv nahrat cele do listu po koniec suboru
+
+// vyratat priemer
+
+// zlozit vzorec
+
+var sum = new SumOperation(
+    new List<Term>() 
+    { 
+        new AbsoluteMember(1),
+        new AbsoluteMember(2),
+    }
+);
+
